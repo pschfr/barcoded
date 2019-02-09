371a1987f38be931538a1887b2557633246a0561
Paul Schaefer <yeppaul2@gmail.com>
2019-02-09 10:32:37 -0500

    I added Tachyons classes, added help text, and prevented some errors from occuring.

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+// Render an example barcode on load
+JsBarcode('#barcode', 'L01A001A01$');
+
+// When changing the input, do this...
 document.getElementById('input').addEventListener('keyup', function() {
-	JsBarcode('#barcode', this.value);
+	// Render a barcode from the input value
+	JsBarcode('#barcode', this.value, {
+		valid: function(valid) {
+			// and if it's invalid, render example barcode
+			if (!valid) {
+				JsBarcode('#barcode', 'L01A001A01$');
+			}
+		}
+	});
 });
