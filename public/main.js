09fdc24b236de9908de7d2dcf5256ce4d3379f16
Paul Schaefer <yeppaul2@gmail.com>
2019-02-09 11:21:52 -0500

    Improved JS, prepared app for Heroku deployment.

--- public/main.js
+++ public/main.js
@@ -1,14 +1,17 @@
+// Easier to do this here rather than 5 other times
+const barcode = document.getElementById('barcode');
+
 // Render an example barcode on load
-JsBarcode('#barcode', document.getElementById('barcode').dataset.default);
+JsBarcode(barcode, barcode.dataset.default);
 
 // When changing the input, do this...
 document.getElementById('input').addEventListener('keyup', function() {
 	// Render a barcode from the input value
-	JsBarcode('#barcode', this.value, {
+	JsBarcode(barcode, this.value, {
 		valid: function(valid) {
 			// and if it's invalid, render example barcode
 			if (!valid) {
-				JsBarcode('#barcode', document.getElementById('barcode').dataset.default);
+				JsBarcode(barcode, barcode.dataset.default);
 			}
 		}
 	});
