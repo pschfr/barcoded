80a01931884de1e48f4d4123d295b3845be2ec41
Paul Schaefer <yeppaul2@gmail.com>
2019-02-12 17:16:38 -0500

    Added query string support in case you hit enter accidentally.

--- public/main.js
+++ public/main.js
@@ -1,18 +1,26 @@
-// Easier to do this here rather than 5 other times
-const barcode = document.getElementById('barcode');
+const element = document.getElementById('barcode');
+const input   = document.getElementById('input');
+const params  = new URLSearchParams(window.location.search);
 
 // Render an example barcode on load
-JsBarcode(barcode, barcode.dataset.default);
+JsBarcode(element, barcode.dataset.default);
 
 // When changing the input, do this...
-document.getElementById('input').addEventListener('keyup', function() {
+input.addEventListener('keyup', function() {
 	// Render a barcode from the input value
-	JsBarcode(barcode, this.value, {
+	JsBarcode(element, this.value, {
 		valid: function(valid) {
 			// and if it's invalid, render example barcode
 			if (!valid) {
-				JsBarcode(barcode, barcode.dataset.default);
+				JsBarcode(element, barcode.dataset.default);
 			}
 		}
 	});
 });
+
+// If there is a query parameter (and it's not empty),
+// set the input value and render a barcode.
+if (params.has('input') && params.get('input') != '') {
+	input.value = params.get('input');
+	JsBarcode(element, params.get('input'));
+}
